ff929e9cb096d7215f024dc40172d7cac44e3388
Slavavechir812 <megatech.work.slava@gmail.com>
2024-11-22 08:03:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/*", async (req, res) => {
     console.log("Target URL requested:", targetUrl);
 
     // Fetch the content from the target URL
+    console.log("Ready URL:", decodeURIComponent(targetUrl))
     const response = await axios.get(decodeURIComponent(targetUrl), {
       headers: {
         "User-Agent": "Node.js Proxy", // Set user-agent
